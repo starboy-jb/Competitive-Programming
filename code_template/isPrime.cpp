b790d249a6ae8585005edfe32450a2461b005c29
Jyotish Bhaskar <35897351+starboy-jb@users.noreply.github.com>
2018-12-23 18:21:57 +0530

    Create isPrime.cpp

---
+++ code_template/isPrime.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll , ll> PLL;
+typedef long double ld;
+
+#define pb push_back
+#define all(c) c.begin(),c.end()
+#define allr(c) c.rbegin(),c.rend()
+#define mod 1000000007
+#define PI 3.14159265
+#define endl '\n'
+#define MAX 
+
+bool isPrime (ll n)
+{
+    if(n == 1) return 0;
+    if(n == 2 or n == 3) return 1;
+    if(n % 2 == 0 or n % 3 == 0) return 0;
+
+    for(ll i = 5; i * i <= n; i += 6) if(n % i == 0 or n % (i + 2) == 0) return 0;
+    return 1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
