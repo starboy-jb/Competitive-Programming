ceffb0933f11cf7a51eba15c73c44b775f24d8ac
Jyotish Bhaskar <35897351+starboy-jb@users.noreply.github.com>
2018-12-23 18:22:21 +0530

    Update isPrime.cpp

--- code_template/isPrime.cpp
+++ code_template/isPrime.cpp
@@ -16,8 +16,7 @@ typedef long double ld;
 #define endl '\n'
 #define MAX 
 
-bool isPrime (ll n)
-{
+bool isPrime (ll n) {
     if(n == 1) return 0;
     if(n == 2 or n == 3) return 1;
     if(n % 2 == 0 or n % 3 == 0) return 0;
