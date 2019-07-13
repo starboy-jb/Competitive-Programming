0d06438f299728cb0125073a1888e33eb0ffc685
Jyotish Bhaskar <35897351+starboy-jb@users.noreply.github.com>
2019-07-13 19:16:29 +0530

    Create purefarzi.cpp

---
+++ purefarzi.cpp
@@ -0,0 +1,106 @@
+/*input
+4
+
+*/
+ 
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <numeric>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll , ll> PLL;
+typedef long double ld;
+ 
+#define pb push_back
+#define all(c) c.begin(),c.end()
+#define allr(c) c.rbegin(),c.rend()
+int mod = 1000000007;
+const int INF = 1034567891;
+const ll LL_INF = 1234567890123456789ll;
+#define PI 3.14159265
+#define endl '\n'
+#define F first
+#define S second
+#define debug(x) cout << #x << " = " << x << endl;
+#define TRACE
+ 
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+#else
+#define trace(...)
+#endif
+ 
+#define out(container) for (auto it : container) cout << it << " "; cout << endl;
+ 
+ 
+template < typename T > T GCD(T a, T b)            { ll t; while(a) { t = a; a = b % a; b = t; } return b; }
+template < typename T > string toString(T a)       { return to_string(a); }
+template < typename T > void toInt(string s, T &x) { stringstream str(s); str >> x;}
+inline int add(int x, int y){ x += y; if(x >= mod) x -= mod; return x;}
+inline int sub(int x, int y){ x -= y; if(x < 0) x += mod; return x;}
+inline int mul(int x, int y){ return (x * 1ll * y) % mod;}
+inline int powr(int a, ll b){
+  int x = 1 % mod;
+  while(b){
+    if(b & 1) x = mul(x, a);
+    a = mul(a, a);
+    b >>= 1;
+  }
+  return x;
+}
+inline int inv(int a){ return powr(a, mod - 2);}
+
+const int N = 1e6;
+int dp[N];
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+ 
+  int n;
+  cin >> n;
+  dp[0] = 1;
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 3;
+  for (int i = 4; i <= n; i++) {
+    dp[i] = add(dp[i], dp[i - 1]);
+    dp[i] = add(dp[i], dp[i - 2]);
+  }
+  int ans = dp[n];
+  for (int i = 0; i <= n - 3; i++) {
+    int left = i;
+    int right = n - (i + 3);
+    int total = mul(dp[left], dp[right]);
+    ans = add(ans, total);
+  }
+  cout << ans << endl;
+
+  return 0;
+}
