3415f607c9f2ede9f780ac30f0627cd7273753fe
Jyotish Bhaskar <35897351+starboy-jb@users.noreply.github.com>
2020-01-19 13:07:52 +0530

    Update DSU.cpp

--- Algorithm/DSU.cpp
+++ Algorithm/DSU.cpp
@@ -19,32 +19,32 @@ typedef long double ld;
 #warning set N accordingly
 
 struct DSU {
-    int connected;
-    int par[N], sz[N];
-    DSU(int n) {
-        for (int i = 1; i <= n; i++) {
-            par[i] = i;
-            sz[i] = 1;
-        }
-        connected = n;
+  int connected;
+  int par[N], sz[N];
+  DSU(int n) {
+    for (int i = 1; i <= n; i++) {
+      par[i] = i;
+      sz[i] = 1;
     }
-    int getPar(int k) {
-        while (k != par[k]) {
-            par[k] = par[par[k]];
-            k = par[k];
-        }
-        return k;
+    connected = n;
+  }
+  int getPar(int k) {
+    while (k != par[k]) {
+      par[k] = par[par[k]];
+      k = par[k];
     }
-    void unite(int u, int v) {
-        int par1 = getPar(u), par2 = getPar(v);
-        if (par1 == par2) return;
-        connected--;
-        if (sz[par1] > sz[par2])
-            swap(par1, par2);
-        sz[par2] += sz[par1];
-        sz[par1] = 0;
-        par[par1] = par[par2];
-    }
+    return k;
+  }
+  void unite(int u, int v) {
+    int par1 = getPar(u), par2 = getPar(v);
+    if (par1 == par2) return;
+    connected--;
+    if (sz[par1] > sz[par2])
+      swap(par1, par2);
+    sz[par2] += sz[par1];
+    sz[par1] = 0;
+    par[par1] = par[par2];
+  }
 };
 
 int main()
