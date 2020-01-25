8d6993832d7420ac9aa2b55eaa0e1c51756fae7e
Jyotish Bhaskar <35897351+starboy-jb@users.noreply.github.com>
2020-01-25 17:41:25 +0530

    Update gen.cpp

--- code_template/gen.cpp
+++ code_template/gen.cpp
@@ -1,61 +1,131 @@
 /*input
 
-
 */
-
-#include <bits/stdc++.h>
-
+ 
+/*
+                                                    ______________
+                                                          |       )
+                                                          |   )   /
+                                                          |______/
+                                                    |     |      \
+                                                    |     |   )   \
+                                                    |_____|_______)
+ 
+*/
+ 
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <queue>
+#include <bitset>
+#include <iomanip>
+ 
 using namespace std;
-
+ 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int,int> PII;
 typedef pair<ll , ll> PLL;
 typedef long double ld;
-
+ 
 #define pb push_back
 #define all(c) c.begin(),c.end()
 #define allr(c) c.rbegin(),c.rend()
 int mod = 1000000007;
+const int inf = 1034567891;
+const ll LL_INF = 1234567890123456789ll;
 #define PI 3.14159265
 #define endl '\n'
+#define F first
+#define S second
 #define debug(x) cout << #x << " = " << x << endl;
-#define MAX 1501
-
+#define TRACE
+ 
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+#else
+#define trace(...)
+#endif
+ 
+#define out(container) for (auto it : container) cout << it << " "; cout << endl;
+ 
+ 
 template < typename T > T GCD(T a, T b)            { ll t; while(a) { t = a; a = b % a; b = t; } return b; }
 template < typename T > string toString(T a)       { return to_string(a); }
 template < typename T > void toInt(string s, T &x) { stringstream str(s); str >> x;}
-inline int add(ll a, ll b) {a += b; if (a < 0) a += mod; return a;}
-inline int sub(ll a, ll b) {a -= b; if (a < 0) a += mod; return a;}
-inline int mul(ll a, ll b) {return (a * 1ll * b) % mod;}
-inline int pwr(ll x, ll y) {
-    int ans = 1 % mod;
-    while (y) {
-        if (y & 1) ans = mul(ans, x);
-        x = mul(x, x);
-        y >>= 1;
-    }
-    return ans;
+inline int add(int x, int y){ x += y; if(x >= mod) x -= mod; return x;}
+inline int sub(int x, int y){ x -= y; if(x < 0) x += mod; return x;}
+inline int mul(int x, int y){ return (x * 1ll * y) % mod;}
+inline int powr(int a, ll b){
+  int x = 1 % mod;
+  while(b){
+    if(b & 1) x = mul(x, a);
+    a = mul(a, a);
+    b >>= 1;
+  }
+  return x;
 }
-inline int inv(int a) {return pwr(a, mod - 2);}
+inline int inv(int a){ return powr(a, mod - 2);}
 
 int rand(int a, int b) {
-    return a + rand() % (b - a + 1);
+  return a + rand() % (b - a + 1);
+}
+
+void genTree(int n) {
+  vector <PII> edges;
+  for (int i = 2; i <= n; i++)
+    edges.pb({rand(1, i - 1), i});
+  vector <int> perm(n + 1);
+  for (int i = 1; i <= n; i++)
+    perm[i] = i;
+  random_shuffle(perm.begin() + 1, perm.end());
+  random_shuffle(all(edges));
+  for (auto it : edges) {
+    int u = it.F;
+    int v = it.S;
+    if (rand() % 2)
+      swap(u, v);
+    cout << perm[u] << " " << perm[v] << endl;
+  }
 }
 
 int main(int argc, char *argv[])
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    srand(atoi(argv[1]));
-    int mx = 20;
-    int test = 5;
-    cout << test << endl;
-    while (test--) {
-        
-    }
+  srand(atoi(argv[1]));
+  int mx = 100;
+  int test = rand(1, 1);
+  // cout << test << endl;
+  while (test--) {
+    int n = rand(2, 1e2);
+    cout << n << endl;
+  }
     
     
-    return 0;
+  return 0;
 }
