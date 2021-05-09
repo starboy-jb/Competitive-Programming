6cc70743fd2ef15693f8f49eafd382d41f829aa8
Jyotish Bhaskar <35897351+starboy-jb@users.noreply.github.com>
2021-05-09 18:10:34 +0530

    Update persistent_segment_tree.cpp

--- Algorithm/persistent_segment_tree.cpp
+++ Algorithm/persistent_segment_tree.cpp
@@ -8,7 +8,7 @@
 1 2 3 10
 2 3 2 3
 2 1 1 10
-
+ 
 */
  
 /*
@@ -44,7 +44,7 @@
 #include <iomanip>
 #include <fstream>
 #include <stack>
-
+ 
 using namespace std;
  
 typedef long long ll;
@@ -100,55 +100,63 @@ inline int powr(int a, ll b){
 }
 inline int inv(int a){ return powr(a, mod - 2);}
 
-struct Node {
-  Node *left, *right;
-  int sum;
-  Node() : left(NULL), right(NULL), sum(0) {}
-  Node(int v) : left(NULL), right(NULL), sum(v) {}
-};
+const int N = 1e5 + 5;
+const int LOG_N = log2(N) + 1;
 
-Node* merge(Node *node1, Node *node2) {
-  Node *node = new Node();
-  node->left = node1;
-  node->right = node2;
-  if (node1) node->sum += node1->sum;
-  if (node2) node->sum += node2->sum;
+int seg[4 * LOG_N * N], lc[4 * LOG_N * N], rc[4 * LOG_N * N], root[N];
+int id = 0;
+
+int build(int low, int high) {
+  int node = id++;
+  if (low == high) {
+    return node;
+  }
+
+  int mid = (low + high) >> 1;
+
+  lc[node] = build(low, mid);
+  rc[node] = build(mid + 1, high);
+
   return node;
 }
 
-Node* build(vector<int>& vec, int low, int high) {
+int update(int prev, int low, int high, int pos, int value) {
+  int node = id++;
   if (low == high) {
-    return new Node(vec[low]);
+    seg[node] = value;
+    return node;
   }
-  int mid = (low + high) >> 1;
-  Node *root = merge(build(vec, low, mid), build(vec, mid + 1, high));
-  return root;
-}
 
-Node* update(Node *root, int low, int high, int pos, int value) {
-  if (low == high) {
-    return new Node(value);
-  }
+  lc[node] = lc[prev];
+  rc[node] = rc[prev];
+
   int mid = (low + high) >> 1;
+
   if (pos <= mid) {
-    return merge(update(root -> left, low, mid, pos, value), root -> right);
+    lc[node] = update(lc[prev], low, mid, pos, value);
   } else {
-    return merge(root -> left, update(root -> right, mid + 1, high, pos, value));
+    rc[node] = update(rc[prev], mid + 1, high, pos, value);
   }
-  return root;
+
+  seg[node] = seg[lc[node]] + seg[rc[node]];
+
+  return node;
 }
 
-int query(Node *root, int low, int high, int l, int r) {
+int query(int root, int low, int high, int l, int r) {
   if (low > r || high < l) return 0;
-  if (low >= l && high <= r) return root -> sum;
-
+  if (low >= l && high <= r) return seg[root];
+ 
   int mid = (low + high) >> 1;
-
-  int left = query(root -> left, low, mid, l, r);
-  int right = query(root -> right, mid + 1, high, l, r);
-  return left + right;
+ 
+  return query(lc[root], low, mid, l, r) + query(rc[root], mid + 1, high, l, r);
 }
 
+void persistentSegTree(int n) {
+  root[0] = build(0, n - 1);
+}
+
+ 
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -162,36 +170,36 @@ int main()
     cin >> v;
     vec.pb(v);
   }
-
-
+  
   int q;
   cin >> q;
 
-  Node *root;
-  vector<Node*> version(q);
+  persistentSegTree(n);
 
-  version[0] = build(vec, 0, n - 1);
-
+  for (int i = 0; i < n; i++) {
+    root[0] = update(root[0], 0, n - 1, i, vec[i]);
+  }
+ 
   int first_type = 1;
-
+ 
   while (q--) {
     int type;
     cin >> type;
-
+ 
     if (type == 1) {
       int idx, pos, v;
       cin >> idx >> pos >> v;
       pos--;
-      v += query(version[idx], 0, n - 1, pos, pos);
-      version[first_type++] = update(version[idx], 0, n - 1, pos, v);
+      v += query(root[idx], 0, n - 1, pos, pos);
+      root[first_type++] = update(root[idx], 0, n - 1, pos, v);
     } else {
       int idx, l, r;
       cin >> idx >> l >> r;
       l--, r--;
-      int ans = query(version[idx], 0, n - 1, l, r);
+      int ans = query(root[idx], 0, n - 1, l, r);
       cout << ans << endl;
     }
   }
-
+ 
   return 0;
-}
+} 
